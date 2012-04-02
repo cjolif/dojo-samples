5d366d400a41e3f5bd126ad2c6f5aa89c92293fb
Christophe Jolif <cjolif@gmail.com>
2012-04-02 16:22:16 +0200

    do not force compat by default

--- mobile-stock/src.js
+++ mobile-stock/src.js
@@ -3,7 +3,7 @@ var details;
 require([
 	"dojox/mobile/parser",
 	"dojox/mobile",
-	"dojox/mobile/compat",
+//	"dojox/mobile/compat",
 	"dojox/mobile/FixedSplitter",
 	"dojox/mobile/ScrollableView",
 	"dojox/mobile/Pane",
