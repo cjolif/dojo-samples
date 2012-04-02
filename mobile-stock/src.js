2d51234053943ee6868ce53f479b88cd759d7404
Christophe Jolif <cjolif@gmail.com>
2012-04-02 16:20:48 +0200

    first draft

---
+++ mobile-stock/src.js
@@ -0,0 +1,17 @@
+var details;
+
+require([
+	"dojox/mobile/parser",
+	"dojox/mobile",
+	"dojox/mobile/compat",
+	"dojox/mobile/FixedSplitter",
+	"dojox/mobile/ScrollableView",
+	"dojox/mobile/Pane",
+	"dojox/mobile/Container",
+	"dojox/mobile/SwapView",
+	"dojox/mobile/PageIndicator"
+], function(){
+	details = function(event){
+		console.log("details: "+event.target.id);
+	};
+});
